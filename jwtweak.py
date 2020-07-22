0161f14695bc7c279a2a9e37778e0edf032e9447
Sudharshan Kumar <sudharshankr94@gmail.com>
2020-07-22 11:06:03 +0200

    Cleaning up, adding remaining comments

--- jwtweak.py
+++ jwtweak.py
@@ -1,7 +1,78 @@
 import jwt
 import re
+import argparse
+import base64
 import json
-from helper_funcs import bcolors, decode_base64, encode_base64
+from Crypto.PublicKey import RSA
+
+"""
+JWTweak-v1.6
+
+1: Detect the algorithm and decode the input JWT Token
+2: Generate new JWT with algorithm ['none','HS256', 'HS384', 'HS512', 'RS256', 'RS384', 'RS512']
+"""
+
+parser = argparse.ArgumentParser(description='JWTeak Tool')
+parser.add_argument('--jwt', nargs='?', help='Input JWT token')
+# parser.add_argument('--detect', nargs='?', help='Detect the algorithm of the input JWT Token')
+# parser.add_argument('--decode', nargs='?', help='Base64 decode the input JWT Token')
+parser.add_argument('--create', nargs='?', help='Create new JWT with new algorithm. Specify algorithm. Default: none')
+parser.add_argument('--payload', nargs='?', help='Provide new payload')
+parser.add_argument('--key', nargs='?', help='Provide symmetric key')
+args = parser.parse_args()
+
+
+class bcolors:
+    """
+    Setting terminal colors
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def RSAKeypairGen():
+    """
+    Creating RSA key pair
+    """
+    new_key = RSA.generate(2048, e=65537)
+    pubKey = new_key.publickey().exportKey("PEM")
+    privKey = new_key.exportKey("PEM")
+    return pubKey, privKey
+
+
+def decode_base64(token, sign=False):
+    """
+    Decode base64 encoded parts of the JWT
+    """
+    token_decoded = ""
+    if len(token) % 4 != 0:  # check if multiple of 4
+        while len(token) % 4 != 0:
+            token = token + "="
+    try:
+        if not sign:
+            token_decoded = str(base64.b64decode(token), 'utf-8')
+        else:
+            token_decoded = str(base64.b64decode(token))
+    except Exception as e:
+        print(f"{bcolors.FAIL}\nThis may not be a JWT Token. Please check again{bcolors.ENDC}\n")
+        print("The following Exception occurred: ", e)
+
+    return token_decoded
+
+
+def encode_base64(json_token):
+    """
+    Encode parts of JWT using base64
+    """
+    str_json_header = json.dumps(json_token)
+    new_header = str(base64.b64encode(str_json_header.encode("utf-8")), 'utf-8')
+    return new_header
 
 
 class JWTweak:
@@ -15,8 +86,8 @@ class JWTweak:
                 # print(f"{bcolors.BOLD}")
                 print()
             else:
-                print("Invalid JWT token")
-                exit(1)
+                raise ValueError("Invalid JWT token")
+
         self._jwt = token
         self._header, self._payload, self._signature = ("", "", "") if not token \
             else map(lambda x: x, self._jwt.split('.'))
@@ -24,7 +95,7 @@ class JWTweak:
 
     def detect(self):
         """
-        Detect algorithm of the given JWT
+        Detect the algorithm of the given JWT
         """
         json_header = json.loads(decode_base64(self._header))
         self._algo = json_header['alg']
@@ -42,19 +113,14 @@ class JWTweak:
 
     def create_new_jwt(self, payload, algo, key):
         """
-        Create new JWT with the passed algorithm, payload and key when applicable
+        Create new JWT with the payload, algorithm and key (if applicable) given in the arguments
         """
-        # json_header = json.loads(decode_base64(self._header))
-        # json_header['alg'] = algo
-        # mod_header = encode_base64(json_header)
-        # mod_payload = json.loads(decode_base64(self._payload))
         mod_payload = ""
         if payload:
             if re.match(r'(.*?)(?:")', payload):
                 mod_payload = json.loads(payload)
             else:
-                print("Not Valid Payload")
-                exit(1)
+                raise ValueError("Not Valid Payload")
         else:
             if self._payload is not None:
                 mod_payload = json.loads(decode_base64(self._payload))
@@ -75,3 +141,18 @@ class JWTweak:
         print("\nThe New JWT Token with Algorithm changed to %s is : %s \n\n" % (algo,
                                                                                  f"{bcolors.OKGREEN}" + f"{bcolors.BOLD}"
                                                                                  + new_jwt + f"{bcolors.OKGREEN}{bcolors.ENDC}"))
+
+
+if __name__ == '__main__':
+    jwt_token = JWTweak(args.jwt)
+    if args.jwt:
+        jwt_token.detect()
+        jwt_token.decode()
+    if args.create:
+        payload = None
+        if args.payload:
+            payload = args.payload
+        try:
+            jwt_token.create_new_jwt(payload, args.create, args.key)
+        except Exception as e:
+            print("Error: ", e)
